ad650a36448208b71701588573e949b0c628e4b7
Mohammad Arhaan <69954942+Arhaan10s@users.noreply.github.com>
2025-01-23 10:48:07 +0530

    Revert "Abhishek"

--- app/Controllers/Auth/login.js
+++ app/Controllers/Auth/login.js
@@ -1,4 +1,5 @@
 const sequelize = require("../../Connection/sequelize");
+const plan = require("../../Models/plan");
 const users = require("../../Models/users");
 
 
--- app/Models/users.js
+++ app/Models/users.js
@@ -2,46 +2,33 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../Connection/sequelize");
 
 const users = sequelize.define("user", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  company_id:{
-    type:DataTypes.STRING,
-    allowNull:false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },  
-  mobile: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("admin", "company", "employee"),
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  created_by: {
-    type: DataTypes.BIGINT,
-    defaultValue: 0,
-  }
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    created_by: {
+        type: DataTypes.BIGINT,
+        defaultValue: 0,
+    },
+    mobile: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
 });
 // sequelize
-//     .sync({alter:true})
+//     .sync()
 //     .then(() => {
 //         console.log("Database & tables created!");
 //     })
 //     .catch((error) => {
 //         console.error("Error creating database & tables:", error);
 //     });
-module.exports = users;
+module.exports = users
