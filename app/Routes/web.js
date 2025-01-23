755fa79bb6588cd1108f212e620a3a1cc2a24a24
abhishekshukla-laravel <dev.abhi07@outlook.com>
2025-01-23 17:27:24 +0530

    Update web.js

--- app/Routes/web.js
+++ app/Routes/web.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const { login } = require("../Controllers/Auth/login");
-
+const router = express.Router();
 const { Register } = require("../Controllers/Auth/register");
-const router = express.Router();
-
 const { companyRegister } = require("../Controllers/Auth/register");
-
-const router = express.Router();
 
 router.post('/login',login)
 router.post('/company-register',companyRegister)
