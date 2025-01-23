5750ef09cd2c51a776b42f181507a718b8c375da
varun383 <156508175+varun383@users.noreply.github.com>
2025-01-23 12:14:28 +0530

    routes

--- app/Routes/web.js
+++ app/Routes/web.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { login } = require("../Controllers/Auth/login");
+const { Register } = require("../Controllers/Auth/register");
 const router = express.Router();
 
 router.post('/login',login)
+router.post('/register',Register)
 module.exports = router;
