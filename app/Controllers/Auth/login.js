29b430901beea0ddea0423d44675bf9389da8fac
abhishekshukla-laravel <dev.abhi07@outlook.com>
2025-01-23 10:41:13 +0530

    new models updated

--- app/Controllers/Auth/login.js
+++ app/Controllers/Auth/login.js
@@ -1,7 +1,5 @@
 const sequelize = require("../../Connection/sequelize");
-const plan = require("../../Models/plan");
 const users = require("../../Models/users");
-
 
 
 exports.login = async (req, res) => {
