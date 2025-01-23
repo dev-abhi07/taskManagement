7359b02a0329c26035eb4c064cb325327d4440bf
varun383 <156508175+varun383@users.noreply.github.com>
2025-01-23 17:36:48 +0530

    Update web.js

--- app/Routes/web.js
+++ app/Routes/web.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const { login } = require("../Controllers/Auth/login");
 
-const { Register } = require("../Controllers/Auth/register");
-const router = express.Router();
-
 const { companyRegister } = require("../Controllers/Auth/register");
 
 const router = express.Router();
