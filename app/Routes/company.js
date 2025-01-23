ff9ad42e1767d0d416fa5583322b512054d439fa
varun383 <156508175+varun383@users.noreply.github.com>
2025-01-23 17:36:43 +0530

    Update company.js

--- app/Routes/company.js
+++ app/Routes/company.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const { register } = require('../Controllers/Company/employee')
+const { createDepartment, getDepartments, updateDepartment, deleteDepartment } = require('../Controllers/Company/department')
 const router = express.Router()
 
 router.post('/register',register)
+router.post('/create-department',createDepartment)
+router.post('/get-department',getDepartments)
+router.post('/update-department',updateDepartment)
+router.post('/delete-department',deleteDepartment)
 
 
 module.exports = router
