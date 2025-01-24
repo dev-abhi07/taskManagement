12724d7977ffa1360789f5a3f39d367e3c6666aa
varun383 <156508175+varun383@users.noreply.github.com>
2025-01-24 15:18:59 +0530

    update designation.js

--- app/Controllers/Company/designation.js
+++ app/Controllers/Company/designation.js
@@ -1,20 +1,22 @@
 const Helper = require("../../../Helper/helper");
+const department = require("../../Models/department");
 const designation = require("../../Models/designation");
 
 
 
 exports.createDesignation = async (req, res) => {
-    const { name} = req.body;
-    const comapany_id = req.headers['x-id'];
+    const { name,department_id,status} = req.body;
+
     try {
         if(!name){
             return Helper.response("failed","Please provide all required fields",[],res,200)
         }
         const designations = await designation.create({
             name: name,
-            company_id: comapany_id,
-            status: true,
-            created_by: 1
+            company_id:req.headers['x-id'],
+            status: status,
+            department_id: department_id,
+            created_by: req.headers['x-id']
         });
         if (designations) {
             return Helper.response("success", "Designation created successfully", designations, res, 200)
@@ -27,20 +29,27 @@ exports.createDesignation = async (req, res) => {
 }
 
 exports.getDesignations = async (req, res) => {
-    const {id} = req.body;
+    const {id,department_id} = req.body;
     try{
-        if(id){
+        if(!department_id || !id){
+            return Helper.response("failed","Please provide all required fields",[],res,200)
+        }
+        if(id && department_id){
             const designationsById = await designation.findOne({where:{id:id}})
+            const departments = await department.findOne({where:{id:department_id}})
+            if(!departments){
+                return Helper.response("failed","No departments found",[],res,200)
+            }
+            const departmentName = departments ? departments.name : null;
+            const data = {
+                departmentName: departmentName,
+                ...designationsById,
+            };
             if(!designationsById){
                 return Helper.response("failed","No designations found",[],res,200)
             }
-            return Helper.response("success","Designations found",designationsById,res,200)
+            return Helper.response("success","Designations found",data,res,200)
         }
-        const designations = await designation.findAll({})
-        if(!designations){
-            return Helper.response("failed","No designations found",[],res,200)
-        }
-        return Helper.response("success","Designations found",designations,res,200)
     }catch(error){
         console.log(error)
         return Helper.response("failed",error,[],res,500)
@@ -71,12 +80,12 @@ exports.updateDesignation = async (req, res) => {
 };
 
 exports.deleteDesignation = async (req,res) => {
-    const {id} = req.body;
+    const {id,department_id} = req.body;
     try{
         if(!id){
             return Helper.response("failed","Please provide all required fields",[],res,200)
         }
-        const designationDelete = await designation.destroy({where:{id:id}})
+        const designationDelete = await designation.destroy({where:{id:id,department_id:department_id}})
         if(designationDelete){
             return Helper.response("success","Designation deleted successfully",designationDelete,res,200)
         }else{
