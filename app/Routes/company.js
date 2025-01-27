441c7ffc04467bbcd6bb1262bacae0706f95d04a
abhishekshukla-laravel <dev.abhi07@outlook.com>
2025-01-27 11:41:26 +0530

    update Conflict and Merge.

--- app/Routes/company.js
+++ app/Routes/company.js
@@ -14,9 +14,6 @@ router.post('/department-list',getDepartments)
 router.post('/update-department',updateDepartment)
 router.post('/delete-department',deleteDepartment)
 
-
-
-
 router.post('/register',register)
 router.post('/register-hierarchy',registerHierarchy)
 
@@ -27,6 +24,5 @@ router.post('/create-designation',createDesignation)
 router.post('/designation-list',designationsList)
 router.post('/update-designation',updateDesignation)
 router.post('/delete-designation',deleteDesignation)
-
 
 module.exports = router
