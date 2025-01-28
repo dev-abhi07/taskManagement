a257e6665011e0f854cc5875410cdce3f7123dc1
abhishekshukla-laravel <dev.abhi07@outlook.com>
2025-01-28 12:01:19 +0530

    Updated new models

---
+++ app/Models/board.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../Connection/sequelize");
+
+const board = sequelize.define('board', {
+    company_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    board_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    board_color: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    board_order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    created_by: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    }
+})
+module.exports = board
