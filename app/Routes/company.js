a39ebeccb327f926d1e4f52b7b986f3f351a147b
varun383 <156508175+varun383@users.noreply.github.com>
2025-01-28 12:03:46 +0530

    Update company.js

--- app/Routes/company.js
+++ app/Routes/company.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const { register } = require('../Controllers/Company/employee')
+const { register, departmenetDesignationBasedEmployee } = require('../Controllers/Company/employee')
 
 const { createDepartment, getDepartments, updateDepartment, deleteDepartment } = require('../Controllers/Company/department')
 const { createDesignation, getDesignations, updateDesignation, deleteDesignation, designationsList } = require('../Controllers/Company/designation')
 const { registerHierarchy } = require('../Controllers/Company/company')
 const router = express.Router()
 
-router.post('/register',register)
-router.post('/create-department',createDepartment)
-router.post('/get-department',getDepartments)
-router.post('/update-department',updateDepartment)
-router.post('/delete-department',deleteDepartment)
-router.post('/create-designation',createDesignation)
-router.post('/designation-list',getDesignations)
-router.post('/update-designation',updateDesignation)
-router.post('/delete-designation',deleteDesignation)
+router.post('/employee-register',register)
+// router.post('/create-department',createDepartment)
+// router.post('/get-department',getDepartments)
+// router.post('/update-department',updateDepartment)
+// router.post('/delete-department',deleteDepartment)
+// router.post('/create-designation',createDesignation)
+// // router.post('/designation-list',getDesignations)
+// router.post('/update-designation',updateDesignation)
+// router.post('/delete-designation',deleteDesignation)
 
 //Department
 router.post('/create-department',createDepartment)
@@ -32,5 +32,8 @@ router.post('/create-designation',createDesignation)
 router.post('/designation-list',designationsList)
 router.post('/update-designation',updateDesignation)
 router.post('/delete-designation',deleteDesignation)
+
+//Employee
+router.post('/get-designation-by-department',departmenetDesignationBasedEmployee)
 
 module.exports = router
